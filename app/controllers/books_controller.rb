c9c2103a4d9dbdde81db50b6080a130c69494d10
kensei1016 <kensei1016ab@gmail.com>
2020-01-06 08:04:13 +0900

    フォームの共通化。EditingBook空欄時のバリデーション追加。

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -30,8 +30,11 @@ class BooksController < ApplicationController
 
   def update
     @book = Book.find(params[:id])
-    @book.update(book_params)
-    redirect_to book_path(@book), notice: 'Book was successfully updated.'
+    if @book.update(book_params)
+      redirect_to book_path(@book), notice: 'Book was successfully updated.'
+    else
+      render :edit
+    end
   end
 
   def destroy
